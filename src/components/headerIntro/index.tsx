256d37ebfa28c51fb748ef7e375a3d0d40de6fba
hilaldfzn <hilalfauzan9@gmail.com>
2025-02-27 01:45:25 +0700

    feat: [GREEN] implement HeaderIntro component

---
+++ src/components/headerIntro/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>MAAMS by Ari Harsono</h1>
+      <p>Metode untuk menelusuri sebab-musabab paling awal</p>
+    </div>
+  );
+}
+
+export default Index
