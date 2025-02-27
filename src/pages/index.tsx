6c2fa39c59d84e6be15dd7148d2a3b265249302a
hilaldfzn <hilalfauzan9@gmail.com>
2025-02-27 09:18:21 +0700

    feat: add MainLayout for consistent page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import FAQ from '../components/faq'
 import HeaderIntro from '../components/headerIntro'
+import MainLayout from '../layout/MainLayout'
 
 const Index = () => {
   return (
-    <>
+    <MainLayout marginOverride='m-0'>
       <HeaderIntro />
       <FAQ />
-    </>
+    </MainLayout>
   )
 }
 
