3d0277880a1c64700e8313195e166844e1789bfc
Lidwina Eurora <lidwinaeurora@Lidwinas-MacBook-Air.local>
2025-03-04 00:11:40 +0700

    chore: configure Jest for testing

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    coverageReporters: ['html', 'text', 'lcov', 'cobertura'],
-    transform: {
-      '^.+\\.(ts|tsx)$': 'babel-jest'
-    },
-    setupFiles: ['dotenv/config']
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  coverageReporters: ['html', 'text', 'lcov', 'cobertura'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  setupFiles: ['dotenv/config'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 }
