c5794dc5690b798b13d86b40000e0bde99826ee2
ryandhika_al <ryandhika.al@ui.ac.id>
2025-03-10 16:19:17 +0700

    [REFACTOR] make axios instance for furture work when already implemented auth

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -5,29 +5,31 @@ const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`
 })
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const access = localStorage.getItem('access')
-    if (access) {
-      if (config.headers) config.headers.authorization = `Bearer ${access}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(new Error(error.message || 'Terjadi kesalahan pada request'))
-  }
-)
+// For future work when we already implement authentication and authorization (in sso)
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const access = localStorage.getItem('access')
+//     if (access) {
+//       if (config.headers) config.headers.authorization = `Bearer ${access}`
+//     }
+//     return config
+//   },
+//   (error) => {
+//     return Promise.reject(new Error(error.message || 'Terjadi kesalahan pada request'))
+//   }
+// )
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response?.status === 401) {
-      toast.error('Sesi anda telah berakhir. Silakan login kembali')
-      localStorage.clear()
-      window.location.href = '/login' // Ganti useRouter() dengan window.location.href
-    }
-    return Promise.reject(new Error(error.message || 'Terjadi kesalahan pada response'))
-  }
-)
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     if (error.response?.status === 401) {
+//       toast.error('Sesi anda telah berakhir. Silakan login kembali')
+//       localStorage.clear()
+//       window.location.href = '/login' // Ganti useRouter() dengan window.location.href
+//     }
+//     return Promise.reject(new Error(error.message || 'Terjadi kesalahan pada response'))
+//   }
+// )
+
 
 export default axiosInstance
