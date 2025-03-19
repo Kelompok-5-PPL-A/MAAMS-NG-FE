63523094232c0cd2da404077dd9731d5ae7c553a
Arya Lesmana <arya.lesmana21@ui.ac.id>
2025-03-19 11:03:01 +0700

    feat: [GREEN] implement login with google button

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import MainLayout from '../../layout/MainLayout'
 
 import maams from '../../assets/maams.png'
+import google from '../../assets/google.png'
 
 const Login: React.FC = () => {
   return (
@@ -14,6 +15,13 @@ const Login: React.FC = () => {
 
       <div className='flex flex-col items-center justify-center'>
         <h1 className='text-2xl font-bold'>Masuk ke Akun</h1>
+      </div>
+
+      <div className='flex flex-col items-center justify-center mt-3'>
+        <button className='md:flex md:gap-2 md:items-center md:justify-center block py-2 bg-yellow-500 text-white md:border-0 md:p-2 md:rounded-xl md:w-80'>
+          <img src={google.src} alt='Google Logo' className='w-10 h-10' />{' '}
+          Masuk dengan Google
+        </button>
       </div>
     </MainLayout>
   )
