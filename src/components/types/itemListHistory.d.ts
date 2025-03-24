dc0209d48b53af855fbb432d67678f7e5b9808b4
ryandhika_al <ryandhika.al@ui.ac.id>
2025-03-25 05:53:11 +0700

    feature: [GREEN] add item list history props

---
+++ src/components/types/itemListHistory.d.ts
@@ -0,0 +1,11 @@
+export interface ItemListHistoryProps {
+    title: string
+    timestamp: string
+    mode: string
+    showModeButton?: boolean
+    showDeleteButton?: boolean
+    user: string
+    idQuestion: string | string[] | undefined
+    tags?: string[]
+  }
+  
