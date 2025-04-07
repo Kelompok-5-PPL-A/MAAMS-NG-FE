eb005547c5bfc6dbfb2e8d2a390581e090d5329e
fikrirmdhna <fikrirmdhna01@gmail.com>
2025-04-07 17:56:00 +0700

    chore: modifying next-auth types

--- src/components/types/next-auth.d.ts
+++ src/components/types/next-auth.d.ts
@@ -1,45 +1,57 @@
-import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth"
 
 declare module "next-auth" {
   interface User extends DefaultUser {
-    uuid: string;
-    first_name: string;
-    last_name: string;
-    date_joined?: string;  
-    is_active: boolean;
-    is_staff: boolean;
-    access_token?: string;  
-    refresh_token?: string; 
+    uuid: string
+    email: string
+    username: string
+    first_name: string
+    last_name: string
+    role: string
+    npm: string
+    angkatan: string
+    date_joined?: string
+    is_active: boolean
+    is_staff: boolean
+    access_token?: string
+    refresh_token?: string
   }
 
   interface Session extends DefaultSession {
     user: {
-      uuid: string;
-      email: string;
-      first_name: string;
-      last_name: string;
-      name?: string;
-      is_active: boolean;
-      is_staff: boolean;
-      access_token?: string;
-      refresh_token?: string;
-    } & DefaultSession["user"];
+      uuid: string
+      email: string
+      username: string
+      first_name: string
+      last_name: string
+      role: string
+      npm: string
+      angkatan: string
+      is_active: boolean
+      is_staff: boolean
+      access_token?: string
+      refresh_token?: string
+    } & DefaultSession["user"]
 
-    access_token?: string;
-    refresh_token?: string;
-    error?: string;
+    access_token?: string
+    refresh_token?: string
+    error?: string
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    uuid: string;
-    email: string;
-    first_name: string;
-    last_name: string;
-    is_active: boolean;
-    is_staff: boolean;
-    access_token?: string;
-    refresh_token?: string;
+    uuid: string
+    email: string
+    username: string
+    first_name: string
+    last_name: string
+    role: string
+    npm: string
+    angkatan: string
+    is_active: boolean
+    is_staff: boolean
+    access_token?: string
+    refresh_token?: string
   }
 }
