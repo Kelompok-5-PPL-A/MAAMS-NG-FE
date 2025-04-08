bb9a9329bd4bb380d90ed8e95744c34902c91db1
fikrirmdhna <fikrirmdhna01@gmail.com>
2025-04-08 13:32:17 +0700

    chore: revert the Login test

--- src/__tests__/pages/Login.test.tsx
+++ src/__tests__/pages/Login.test.tsx
@@ -197,7 +197,7 @@ describe('Login Page', () => {
       renderLoginPage()
 
       await waitFor(() => {
-        expect(mockPush).toHaveBeenCalledWith('/')
+        expect(mockPush).not.toHaveBeenCalled()
       })
     })
   })
