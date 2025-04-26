659109afcd4e7442d95c802783657fffc9f80ad8
fikrirmdhna <fikrirmdhna01@gmail.com>
2025-04-26 13:23:06 +0700

    feat: [GREEN] Implement fetch filter and fetch question function

---
+++ src/actions/fetchQuestion.ts
@@ -0,0 +1,34 @@
+import { formatTimestamp } from '../utils/dateFormatter'
+import { Item } from '../components/types/historyPage'
+import axiosInstance from '@/services/axiosInstance'
+
+interface historyData {
+  count: number
+  previous: string
+  next: string
+  results: Item[]
+}
+
+export const fetchQuestions = async (time_range?: string, additional_param?: string) => {
+  let url = `/question/history/${additional_param}&time_range=${time_range}`
+
+  const response = await axiosInstance.get(url)
+
+  const data: historyData = response.data
+  console.log(data)
+
+  const processedData: Item[] = data.results.map((item: any) => ({
+    id: item.id,
+    title: item.question,
+    displayed_title: item.title,
+    timestamp: formatTimestamp(item.created_at),
+    mode: item.mode,
+    user: item.username,
+    tags: item.tags
+  }))
+
+  return {
+    count: data.count,
+    processedData: processedData
+  }
+}
